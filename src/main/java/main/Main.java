704b2afddce4dcb79366b079383e06a5fccd2cac
osparking <jbpark03@gmail.com>
2023-09-01 23:08:18 +0900

    make comment and publish thru service

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,20 @@
 package main;
 
+import model.Comment;
+import proxies.EmailCommentNotificationProxy;
+import repositories.DBCommentRepository;
+import services.CommentService;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		var repository = new DBCommentRepository();
+		var notificationProxy = new EmailCommentNotificationProxy();
+		var service = new CommentService(repository, notificationProxy);
+		var comment = new Comment();
+		comment.setAuthor("개발자");
+		comment.setText("프로젝트는 잘 될거야!");
+		service.publishComment(comment);
 	}
 
 }
