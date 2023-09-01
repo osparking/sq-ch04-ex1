c8ff160fb23840c10ffbcc8d414f220afb02c34a
osparking <jbpark03@gmail.com>
2023-09-01 22:10:38 +0900

    a commentRepository implementation

---
+++ src/main/java/repositories/DBCommentRepository.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import model.Comment;
+
+public class DBCommentRepository implements CommentRepository {
+
+	@Override
+	public void storeComment(Comment comment) {
+		System.out.println("comment is being stored : " + comment.getText());
+	}
+
+}
