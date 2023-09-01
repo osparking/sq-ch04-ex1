e11706e4e584e51d811aba0a6ed90814a25682f2
osparking <jbpark03@gmail.com>
2023-09-01 23:54:49 +0900

    a repository object registered to context

--- src/main/java/repositories/DBCommentRepository.java
+++ src/main/java/repositories/DBCommentRepository.java
@@ -1,8 +1,11 @@
 package repositories;
 
+import org.springframework.stereotype.Component;
+
 import model.Comment;
 import utility.InOut;
 
+@Component
 public class DBCommentRepository implements CommentRepository {
 
 	@Override
