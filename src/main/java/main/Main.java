daa5720bf8dc4ef27915cf36f229e5a69ddf5234
osparking <jbpark03@gmail.com>
2023-09-02 23:53:42 +0900

    main code reduced thanks to the spring

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,23 @@
 package main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import config.ProjectConfig;
 import model.Comment;
-import proxies.EmailCommentNotificationProxy;
-import repositories.DBCommentRepository;
 import services.CommentService;
 
 public class Main {
 
 	public static void main(String[] args) {
-		var repository = new DBCommentRepository();
-		var notificationProxy = new EmailCommentNotificationProxy();
-		var service = new CommentService(repository, notificationProxy);
+		var ctx = new AnnotationConfigApplicationContext(ProjectConfig.class);
+//		var repository = new DBCommentRepository();
+//		var notificationProxy = new EmailCommentNotificationProxy();
+//		var service = new CommentService(repository, notificationProxy);
+		CommentService service = ctx.getBean(CommentService.class);
+		
 		var comment = new Comment();
-		comment.setAuthor("개발자");
-		comment.setText("프로젝트는 잘 될거야!");
+		comment.setAuthor("개발고수");
+		comment.setText("프로젝트는 정말 잘 될거야!");
 		service.publishComment(comment);
 	}
 
